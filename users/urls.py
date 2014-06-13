bcbcb02346a13b827056cf73da861d3418a85bb4
Abhas Mittal <mittalabhas1@gmail.com>
2014-06-13 18:39:44 +0530

    feat(user): removed trailing slash from userdetails

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from users import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.UserList.as_view()),
-	url(r'^(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+	url(r'^(?P<pk>[0-9])', views.UserDetail.as_view()),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
